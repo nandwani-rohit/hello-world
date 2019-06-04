60f5d1659d088b6ecf24aa72a57fddc2c71b192b
Rohit Priyanka Nandwani <nandwani@iitg.ac.in>
2019-06-04 16:58:00 +0530

    Woo hit!

--- tryyyyy.py
+++ tryyyyy.py
@@ -3,3 +3,8 @@ def hi():
 	name = input("What's your name, Officer?")
 	print("Well, hello Officer " + name + "[Smarty Pants]")
 	
+def divide(x, y):
+	"""Divides x by y and returns the
+	float value x/y """
+	return x/y
+
